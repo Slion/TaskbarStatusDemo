3cb4c1144c24c70851de554df6bfcaf7b7cef3d0
Stéphane Lenclud <github@lenclud.com>
2025-06-27 13:31:00 +0200

    First contribution

---
+++ resource.h
@@ -0,0 +1,21 @@
+#define IDS_APP_TITLE                   100
+
+#define IDI_APP                         101
+#define IDI_OVERLAY1                    102
+#define IDI_OVERLAY2                    103
+#define IDC_PERIPHERALSTATUS            104
+
+#define IDM_EXIT                        40001
+#define IDM_OVERLAY1                    40002
+#define IDM_OVERLAY2                    40003
+#define IDM_OVERLAY_CLEAR               40004
+#define IDM_SIMULATEPROGRESS            40005
+#define IDM_PROGRESS_STOP               40006
+#define IDM_PROGRESS_RESUME             40007
+#define IDM_PROGRESS_NONE               40010
+#define IDM_PROGRESS_INDETERMINATE      40011
+#define IDM_PROGRESS_NORMAL             40012
+#define IDM_PROGRESS_ERROR              40013
+#define IDM_PROGRESS_PAUSED             40014
+
+
